fd50160ce7b577c24550b6b2fc2809937ef9bd4a
Bogdan Radchenko <inroph@mail.ru>
2023-02-07 22:27:39 +0300

    Adding object on storyboard

--- ChooseColor/ViewController.swift
+++ ChooseColor/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeRedValue() {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+    }
+    
+    @IBAction func changeGreenValue() {
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+    }
+    
+    @IBAction func changeBlueValue() {
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
 }
 
